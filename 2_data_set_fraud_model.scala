419e1c2ec7f0192f92df8f05d8adc1d65502da8f
David Arango Sampayo <39955504+daasampa@users.noreply.github.com>
2018-06-06 13:24:01 -0500

    Update 2_data_set_fraud_model.scala
    SQL sentence to call input data.

--- 2_data_set_fraud_model.scala
+++ 2_data_set_fraud_model.scala
@@ -1,1 +1,2 @@
+/*This values is just a SQL string sentence that eventually calls, through the spark.sql() method, the input data.*/
 val sql_lz = "select * from proceso_seguridad_externa.scoring_variables_preferenciales order by documento"
