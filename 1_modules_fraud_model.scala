f0030fee4e4536d64c6cbf51d374967279b50fae
David Arango Sampayo <39955504+daasampa@users.noreply.github.com>
2018-06-06 13:20:11 -0500

    Update 1_modules_fraud_model.scala
    The import of Scala classes.

--- 1_modules_fraud_model.scala
+++ 1_modules_fraud_model.scala
@@ -1,3 +1,5 @@
+/*Scala classes needed to transform data and implement the estimation procedure for the probabilistic clustering methods
+using the stage-defined pipeline model.*/
 import org.apache.spark.ml.linalg.Vector
 import org.apache.spark.ml.feature.VectorAssembler 
 import org.apache.spark.ml.feature.MinMaxScaler
